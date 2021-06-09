942801446358db6d9609911593c8c035714c3438
William Lêdo <williamledoc@gmail.com>
2021-06-09 17:05:24 -0300

    OrderItem, many-to-manu association with extra attributes PART 2

--- src/main/java/com/educandoweb/course/repositories/OrderItemRepository.java
+++ src/main/java/com/educandoweb/course/repositories/OrderItemRepository.java
@@ -2,12 +2,10 @@ package com.educandoweb.course.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.educandoweb.course.entities.Category;
 import com.educandoweb.course.entities.OrderItem;
-import com.educandoweb.course.entities.Product;
 import com.educandoweb.course.entities.User;
 
-public interface ProductRepository extends JpaRepository<Product, Long>{
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long>{
 
 	
 	
