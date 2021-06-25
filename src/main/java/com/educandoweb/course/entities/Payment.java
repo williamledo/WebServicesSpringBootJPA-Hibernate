ff7064f341bea05eb3157f218c26d75796481f37
William Lêdo <williamledoc@gmail.com>
2021-06-25 14:59:03 -0300

    Subtotal & Total methods

--- src/main/java/com/educandoweb/course/entities/Payment.java
+++ src/main/java/com/educandoweb/course/entities/Payment.java
@@ -11,6 +11,8 @@ import javax.persistence.MapsId;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 @Entity
 @Table(name = "tb_payment")
 public class Payment implements Serializable {
@@ -21,7 +23,8 @@ public class Payment implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private Instant moment;
-	 
+	
+	@JsonIgnore
 	@OneToOne
 	@MapsId
 	private Order order;
